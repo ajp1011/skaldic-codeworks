0b6d1e74bb87d392a74779fba406f1a5c0510d1c
AJ Perry <ajp1011@gmail.com>
2025-10-10 13:23:16 -0400

    Added favicon

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,8 +4,9 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <title>@yield('title', config('app.name', 'Laravel'))</title>
+        <link rel="icon" type="image/x-icon" href="{{ asset('favicon.ico') }}">
         @vite(['resources/css/app.css', 'resources/js/app.ts'])
-                @stack('styles')
+        @stack('styles')
     </head>
 <body>
     @stack('body-start')
