91e2d1b56a2f90c9e1f9bc5b2d01134de92db489
AJ Perry <ajp1011@gmail.com>
2025-10-10 12:50:09 -0400

    Added linter configuration and linted existing code

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,17 +4,12 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <title>@yield('title', config('app.name', 'Laravel'))</title>
-
-        <!-- Styles / Scripts -->
         @vite(['resources/css/app.css', 'resources/js/app.ts'])
-        
-        @stack('styles')
+                @stack('styles')
     </head>
 <body>
     @stack('body-start')
-    
     @yield('content')
-
     @stack('scripts')
 </body>
 </html>
