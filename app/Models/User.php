51dfbe36b9eef6d3cde46c5de65fde8f212e6867
AJ Perry <ajp1011@gmail.com>
2025-10-30 17:12:38 -0400

    Updated deployment workflow to use 'production' environment

--- app/Models/User.php
+++ app/Models/User.php
@@ -4,24 +4,17 @@ declare(strict_types=1);
 
 namespace App\Models;
 
-// use Illuminate\Contracts\Auth\MustVerifyEmail;
-use App\Enums\UserType;
+use App\Enums\{UserType, Theme};
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
 class User extends Authenticatable
 {
-    /** @use HasFactory<\Database\Factories\UserFactory> */
     use HasFactory;
 
     use Notifiable;
 
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var list<string>
-     */
     protected $fillable = [
         'name',
         'email',
@@ -30,76 +23,41 @@ class User extends Authenticatable
         'theme',
     ];
 
-    /**
-     * The attributes that should be hidden for serialization.
-     *
-     * @var list<string>
-     */
     protected $hidden = [
         'password',
         'remember_token',
     ];
 
-    /**
-     * Get the attributes that should be cast.
-     *
-     * @return array<string, string>
-     */
     protected function casts(): array
     {
         return [
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
             'user_type' => UserType::class,
-            'theme' => \App\Enums\Theme::class,
+            'theme' => Theme::class,
         ];
     }
 
-    /**
-     * Check if the user is a super administrator.
-     *
-     * @return bool
-     */
     public function isSuperAdministrator(): bool
     {
         return $this->user_type === UserType::SUPER_ADMINISTRATOR;
     }
 
-    /**
-     * Check if the user is an administrator.
-     *
-     * @return bool
-     */
     public function isAdministrator(): bool
     {
         return $this->user_type === UserType::ADMINISTRATOR;
     }
 
-    /**
-     * Check if the user is a regular user.
-     *
-     * @return bool
-     */
     public function isUser(): bool
     {
         return $this->user_type === UserType::USER;
     }
 
-    /**
-     * Check if the user is a guest.
-     *
-     * @return bool
-     */
     public function isGuest(): bool
     {
         return $this->user_type === UserType::GUEST;
     }
 
-    /**
-     * Check if the user has admin privileges (super admin or admin).
-     *
-     * @return bool
-     */
     public function hasAdminPrivileges(): bool
     {
         return in_array(
@@ -108,11 +66,6 @@ class User extends Authenticatable
         );
     }
 
-    /**
-     * Get the theme slug for the user.
-     *
-     * @return string
-     */
     public function getThemeSlug(): string
     {
         return $this->theme->slug();
