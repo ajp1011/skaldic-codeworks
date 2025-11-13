647a160f086ae4b062797fcee7907c74d604c91f
AJ Perry <ajp1011@gmail.com>
2025-11-13 00:04:09 -0500

    Improved initial database seeding with a super admin user

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,9 +4,10 @@ declare(strict_types=1);
 
 namespace Database\Seeders;
 
+use App\Enums\UserType;
 use App\Models\User;
-// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -15,11 +16,11 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
-
         User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+            'name' => 'Super Administrator',
+            'user_type' => UserType::SUPER_ADMINISTRATOR,
+            'email' => 'superadmin@example.com',
+            'password' => Hash::make(config('users.default_admin_password', 'password')),
         ]);
     }
 }
